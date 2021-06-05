f81d38b6ffabcb2fe54c25f49d63ddc872961bae
chi <chinedu.onyeoka@gmail.com>
2021-06-06 00:40:59 +0100

    navigator

---
+++ src/Navigations/AuthNavigation.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {createStackNavigator} from '@react-navigation/stack'
+
+import {Text, View} from 'react-native'
+
+const Login = () => {
+  return (<View>
+    <Text>Hi from contacts</Text>
+  </View>)
+}
+
+const Signup = () => {
+  return (<View>
+    <Text>Hi from contact detail</Text>
+  </View>)
+}
+
+
+const AuthNavigator = () => {
+  const AuthStack=createStackNavigator()
+  return (
+    <AuthStack.Navigator>
+    <AuthStack.Screen name="Login" component={Login}></AuthStack.Screen>
+    <AuthStack.Screen name="Register" component={Signup}></AuthStack.Screen>
+        </AuthStack.Navigator>
+  )
+}
+
+export default AuthNavigator;
---
+++ src/Navigations/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import {NavigationContainer} from '@react-navigation/native'
+import {Text} from 'react-native'
+
+const AppNavContainer = () => {
+  return (
+    <NavigationContainer >
+        <Text>Hello World</Text>
+       </NavigationContainer>
+  )
+}
+
+export default AppNavContainer;
