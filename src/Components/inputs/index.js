b878a1e7376a8045e87e52718ac3febea6cf9d7a
chi <chinedu.onyeoka@gmail.com>
2021-06-08 15:54:23 +0100

    input screen

--- src/Components/inputs/index.js
+++ src/Components/inputs/index.js
@@ -2,9 +2,16 @@ import React from 'react'
 import { View, Text } from 'react-native'
 import { TextInput } from 'react-native-gesture-handler'
 
-const Input = ({onChangeText, style, value}) => {
+import styles from './styles'
+
+const Input = ({onChangeText, icon, style, value, label}) => {
   return (
     <View>
+
+    {label && <Text>{label}</Text>}
+
+    <View>{icon && icon}</View>
+
            <TextInput
       style={[style, style.TextInput]}
       onChangeText={onChangeText}
--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Text, TextInput, View} from 'react-native'
 import Container from '../../Components/Container';
+import Input from '../../Components/inputs';
 
 
 
@@ -8,8 +9,8 @@ const Login = () => {
   const [value, onChangeText] =React.useState('Useless placeholder')
   return (
     <Container>
-      <Text>Hi from contacts</Text>
-      <TextInput
+      <Input
+      label="Username"
       style={{height: 40, borderColor: 'brown', borderWidth: 1}}
       onChangeText={(text) => onChangeText(text)}
       value={value}
